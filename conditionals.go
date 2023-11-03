4c294688742dc7013614ef34533461ea0c423afa
Code Mountains <thecodemountains@gmail.com>
2023-11-03 17:47:17 -0500

    else if

--- conditionals.go
+++ conditionals.go
@@ -5,11 +5,13 @@ import "fmt"
 func main(){
 	ages := map[string]int{}
 
-	ages["Kevin"] = 61 
+	ages["Kevin"] = 33 
 
-	if ages["Kevin"] < 67 {
+	if ages["Kevin"] < 18 {
+		fmt.Println("Kevin can't vote")
+	} else if ages["Kevin"] < 67 {
 		fmt.Println("Kevin is not of retirement age")
 	} else {
-		fmt.Println("Kevin is of retirement age")
+		fmt.Println("Enjoy retirement Kevin!")
 	}
 }
