85462145b5879b3d11a8ca6c02e96d051d47fcd1
Code Mountains <thecodemountains@gmail.com>
2023-11-03 17:31:24 -0500

    maps.go

---
+++ maps.go
@@ -0,0 +1,13 @@
+package main 
+
+import "fmt" 
+
+func main() {
+	birthdays := map[string]string{
+		"Keith": "02/02/1990",
+		"Kiran": "04/19/1995",
+		"Kayla": "06/24/1975",
+	}
+
+	fmt.Println(birthdays)
+}
