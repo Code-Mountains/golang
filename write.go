6040bbfe74e785b0f08fc7de0ba790760ca50cf7
Code Mountains <thecodemountains@gmail.com>
2023-11-03 19:36:44 -0500

    write.go

---
+++ write.go
@@ -0,0 +1,38 @@
+package main 
+
+import (
+	"bufio"
+	"fmt"
+	// "io/ioutil"
+	"motd/message"
+	"os"
+	"strings"
+)
+
+func main() {
+	f, err := os.OpenFile("/etc/motd", os.O_WRONLY, 0644)
+
+	if err != nil {
+		fmt.Println("Error: Unable to open /etc/motd")
+		os.Exit(1)
+	}
+
+	defer f.Close()
+
+	reader := bufio.NewReader(os.Stdin) 
+	fmt.Print("Your Greeting: ")
+	phrase, _ := reader.ReadString('\n')
+	phrase = strings.TrimSpace(phrase) 
+
+	fmt.Print("Your Name: ")
+	name, _ := reader.ReadString('\n')
+	name = strings.TrimSpace(name) 
+
+	m := message.Greeting(name, phrase) 
+	_, err = f.Write([]byte(m))
+
+	if err != nil {
+		fmt.Println("Error: Failed to write /etc/motd")
+		os.Exit(1)
+	}
+}
